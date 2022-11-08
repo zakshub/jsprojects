1f6fc4bd193e3a459c131c69d305c1a929c72315
zak@engineer.com <zuhaib.akram@veeonehealth.com>
2022-11-08 19:23:25 +0500

    if and else

--- 18 - decision making/script.js
+++ 18 - decision making/script.js
@@ -1,7 +1,12 @@
-const age = 19;
-const isOldEnough = age >= 18;
+const age = 11;
+// const isOldEnough = age >= 18;
 
-if(isOldEnough){
-  console.log('Sara Can start driving license 🚗');
+if( age >= 15){
+  // console.log('Sara Can start driving license 🚗');
   alert('Sara Can start driving license 🚗');
 }
+  else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young , wait another ${yearsLeft} years:)`);
+    alert(`Sarah is too young , wait another ${yearsLeft} years:)`);
+  }
